58befba209b93e06c4329fc32379d07dcbc8dc44
masato-ka <jp6uzv@gmail.com>
2020-01-12 15:53:32 +0900

    optimize config parameter.

--- train_donkey.py
+++ train_donkey.py
@@ -50,7 +50,11 @@ if __name__ == '__main__':
     env = gym.make('donkey-generated-roads-v0')
     vae_env = VaeEnv(env, vae, device=torch_device, reward_callback=calc_reward)
 
+    '''
+    Normal SAC in stable baselines but code is changed to calculate gradient only when done episode.
+    In gym_donkey, skip_frame parameter is 2 but modify to 1. 
+    '''
     model = SAC(CustomSACPolicy, vae_env, verbose=1, batch_size=64, buffer_size=30000, learning_starts=300,
-                gradient_steps=300, train_freq=1, ent_coef='auto_0.1', learning_rate=0.0003)
+                gradient_steps=600, train_freq=1, ent_coef='auto_0.1', learning_rate=0.0003)
     model.learn(total_timesteps=10000, log_interval=1)
     model.save('donkey4')
