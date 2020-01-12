140389e5b5254e6fca70605f00c43cf280aabe63
masato-ka <jp6uzv@gmail.com>
2020-01-12 14:50:58 +0900

    change reset function.

--- env/vae_env.py
+++ env/vae_env.py
@@ -76,8 +76,9 @@ class VaeEnv(Env):
         #self._wrapped_env.env.t = 1.0
         #observe, _, _, _ = self._wrapped_env.step(self._wrapped_env.action_space.sample())
         o = self._vae(observe)
-        oc = np.concatenate([o, np.asarray(self.action_history)], 0)
-        return oc
+        if self.n_command_history > 0:
+            o = np.concatenate([o, np.asarray(self.action_history)], 0)
+        return o
 
     def step(self, action):
         #Convert from [-1, 1] to [0, 1]
@@ -92,6 +93,7 @@ class VaeEnv(Env):
 
         self._record_action(action)
         observe, reward, done, e_i = self._wrapped_env.step(action)
+
         if np.math.fabs(e_i['cte']) > 2:
             done = True
         else:
